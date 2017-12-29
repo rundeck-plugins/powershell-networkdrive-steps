dcd220e6b54ebf0bedd14b2ab49603fbdd6291a8
miguelantonio <miguel@variacode.com>
2017-12-29 15:05:50 -0300

    Mapping/UnMapping drives on powershell

--- contents/MapNetworkDrive.ps1
+++ contents/MapNetworkDrive.ps1
@@ -1,1 +1,52 @@
 #Requires -Version 4
+<#
+.SYNOPSIS
+  Sandbox Create
+.DESCRIPTION
+  Map a drive from path
+.PARAMETER DriveLetter
+    DriveLetter
+.PARAMETER Path
+    The Path
+.NOTES
+  Version:        1.0.0
+  Author:         Rundeck
+  Creation Date:  12/12/2017
+  
+#>
+
+Param (
+	[string]$DriveLetter,
+	[string]$Path
+)
+
+Begin {
+	try{
+
+		If (-Not ( Test-Path $Path) )
+		{
+			Write-Error "Error: destination path doesn't exists"
+        	exit 1
+		}
+
+		Write-Host "DriveLetter: $($DriveLetter)"
+		Write-Host "Path directory: $($Path)"
+
+
+		If ((Test-Path "$($DriveLetter):"))
+		{
+			Write-Host "Drive already exists, skipping mapping"
+
+		}else{
+			New-PSDrive -Persist  -Scope Global  -Name $DriveLetter -PSProvider "FileSystem" -Root $Path
+
+		} 		
+
+	}Catch{
+        Write-Error "Error: $($_.Exception.Message) - Line Number: $($_.InvocationInfo.ScriptLineNumber)"
+        exit 1
+    }
+
+
+
+}
--- contents/UnmapNetworkDrive.ps1
+++ contents/UnmapNetworkDrive.ps1
@@ -1,1 +1,42 @@
 #Requires -Version 4
+<#
+.SYNOPSIS
+  Sandbox Create
+.DESCRIPTION
+  Map a drive from path
+.PARAMETER DriveLetter
+    DriveLetter
+.NOTES
+  Version:        1.0.0
+  Author:         Rundeck
+  Creation Date:  12/12/2017
+  
+#>
+
+Param (
+	[string]$DriveLetter
+)
+
+Begin {
+	try{
+
+		Write-Host "Removing DriveLetter: $($DriveLetter)"
+
+		If (-Not (Test-Path "$($DriveLetter):"))
+		{
+			Write-Host "Drive doesn't exists, skipping removing"
+			exit 0
+
+		}
+
+ 		Remove-PSDrive -Name $DriveLetter
+
+
+	}Catch{
+        Write-Error "Error: $($_.Exception.Message) - Line Number: $($_.InvocationInfo.ScriptLineNumber)"
+        exit 1
+    }
+
+
+
+}
